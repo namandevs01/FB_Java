14fa43d0f12d6e8ab77f73edf085c6288e0060a1
namandevs01 <namansh.devs@gmail.com>
2025-08-13 09:16:47 +0530

    Basic Print Statement

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Naman");
+        System.out.println("Btech CSE");
+        System.out.println("2415001009");
+    }
+}
